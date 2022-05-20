a0e1ced18d3674ad2f6f51900263a9a062758f35
radugerm <92944144+radugerm@users.noreply.github.com>
2022-05-20 19:34:43 +0200

    Add files via upload

---
+++ Ordinal_Numbers.py
@@ -0,0 +1,4 @@
+th = ["first","second","third","fourth","fifth","sixth","seventh","eighth","ninth","tenth",
+        "eleventh","twelfth","thirteenth","fourteenth","fifteenth","sixteenth","seventeenth","eighteenth","nineteenth","twentieth","twenty-first","twenty-second","twenty-third",
+        "twenty-fourth","twenty-fifth","twenty-sixth","twenty-seventh","twenty-eighth","twenty-ninth",
+          "thirtieth","thirty-first"]
